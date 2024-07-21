e28c55d3cb1f01bc2771e6cdd9ae8c5474001c0e
Nguyen Trung Nghia with Nan <ntrungnghia102@gmail.com>
2024-07-21 22:59:35 +0700

    add select mode system

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,48 @@
 import './App.css';
 import { useColorScheme } from '@mui/material/styles';
 import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness';
+
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme();
+  const handleChange = (event) => {
+    // setAge(event.target.value);
+    setMode(event.target.value);
+  };
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+      <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+      <Select
+        labelId="label-select-dark-light-mode"
+        id="select-dark-light-mode"
+        label="Mode"
+        value={mode}
+        onChange={handleChange}
+      >
+        <MenuItem value="light">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <LightModeIcon fontSize="small" /> Light
+          </Box>
+        </MenuItem>
+        <MenuItem value="dark">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize="small" /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value="system">
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <SettingsBrightnessIcon fontSize="small" /> System
+          </Box>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
+
 function ModeToggle() {
   const { mode, setMode } = useColorScheme();
   return (
@@ -17,6 +59,7 @@ function ModeToggle() {
 function App() {
   return (
     <div>
+      <ModeSelect></ModeSelect>
       <ModeToggle></ModeToggle>
       <Button variant="contained">Hello world</Button>
     </div>
