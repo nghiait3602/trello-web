a3e04f876e5a4bbf9dfca602132498dfe6bf7f64
nghia <ntrungnghia102@gmail.com>
2024-10-17 10:13:04 +0700

    add giu cho khi keo

--- src/pages/Board/BoardContent/BoardContent.jsx
+++ src/pages/Board/BoardContent/BoardContent.jsx
@@ -8,9 +8,18 @@ import {
   useSensors,
   MouseSensor,
   TouchSensor,
+  DragOverlay,
+  defaultDropAnimationSideEffects,
 } from "@dnd-kit/core";
 import { useEffect, useState } from "react";
 import { arrayMove } from "@dnd-kit/sortable";
+import Column from "./ListColumns/Column/Column";
+import TrelloCard from "./ListColumns/Column/ListCards/Card/Card";
+
+const ACTIVE_DRAG_ITEM_TYPE = {
+  COLUMN: "ACTIVE_DRAG_ITEM_TYPE_COLUMN",
+  CARD: "ACTIVE_DRAG_ITEM_TYPE_CARD",
+};
 
 function BoardContent({ board }) {
   const pointerSensor = useSensor(PointerSensor, {
@@ -27,12 +36,26 @@ function BoardContent({ board }) {
   // const sensors = useSensors(pointerSensor);
   // ưu tiên sử dụng mousesensor và touchsensor để trải nghiệm trên mobile tốt nhất khong bug
   const sensors = useSensors(mouseSensor, touchsSensor);
-
   const [orderedColumns, setOrderedColumn] = useState([]);
+  // cùng 1 thời điểm chỉ có 1 phần tử được kéo, -> xữ lý giữ chổ khi xóa
+  const [activeDragItemId, setActiveDragItemId] = useState(null);
+  const [activeDragItemStype, setActiveDragItemStype] = useState(null);
+  const [activeDragItemData, setActiveDragItemData] = useState(null);
+
+  const handleDragStart = (event) => {
+    console.log(event);
+    //set dữ liệu kéo
+    setActiveDragItemId(event?.active?.id);
+    setActiveDragItemStype(
+      event?.active?.data?.current?.columnId
+        ? ACTIVE_DRAG_ITEM_TYPE.CARD
+        : ACTIVE_DRAG_ITEM_TYPE.COLUMN
+    );
+    setActiveDragItemData(event?.active?.data?.current);
+  };
 
   const handleDragEnd = (event) => {
     const { active, over } = event;
-
     if (!over) return;
 
     if (active.id != over.id) {
@@ -40,20 +63,37 @@ function BoardContent({ board }) {
       const newIndex = orderedColumns.findIndex((c) => c._id === over.id); // tìm vị trí over
 
       const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex);
-
       // const dndOrderedColumnIds = dndOrderedColumns.map((c) => c._id);
       // console.log('dndOrderColumn',dndOrderedColumns)
       // console.log('dndOrderColumnIds',dndOrderedColumnIds)
-
       setOrderedColumn(dndOrderedColumns);
+
+      //set dữ liệu khi thả = null
+      setActiveDragItemId(null);
+      setActiveDragItemStype(null);
+      setActiveDragItemData(null);
     }
   };
 
+  const dropAnimationCustum = {
+    sideEffects: defaultDropAnimationSideEffects({
+      styles: {
+        active: {
+          opacity: "0.5",
+        },
+      },
+    }),
+  };
   useEffect(() => {
     setOrderedColumn(mapOrder(board?.columns, board?.columnOrderIds, "_id"));
   }, [board]);
+
   return (
-    <DndContext onDragEnd={handleDragEnd} sensors={sensors}>
+    <DndContext
+      sensors={sensors}
+      onDragEnd={handleDragEnd}
+      onDragStart={handleDragStart}
+    >
       <Box
         sx={{
           bgcolor: (theme) =>
@@ -64,6 +104,16 @@ function BoardContent({ board }) {
         }}
       >
         <ListColumns columns={orderedColumns} />
+        {/* giữ chổ khi kéo */}
+        <DragOverlay dropAnimation={dropAnimationCustum}>
+          {!activeDragItemStype && null}
+          {activeDragItemStype === ACTIVE_DRAG_ITEM_TYPE.COLUMN && (
+            <Column column={activeDragItemData} />
+          )}
+          {activeDragItemStype === ACTIVE_DRAG_ITEM_TYPE.CARD && (
+            <TrelloCard card={activeDragItemData} />
+          )}
+        </DragOverlay>
       </Box>
     </DndContext>
   );
--- src/pages/Board/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Board/BoardContent/ListColumns/Column/Column.jsx
@@ -6,7 +6,7 @@ import {
   Menu,
   MenuItem,
 } from "@mui/material";
-import { Cloud, ContentCut } from "@mui/icons-material";
+import { Cloud, ContentCut, Opacity } from "@mui/icons-material";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
 import { useState } from "react";
@@ -23,145 +23,154 @@ import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 
 function Column({ column }) {
-  const { attributes, listeners, setNodeRef, transform, transition } =
-    useSortable({ id: column._id, data: { ...column } });
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id: column._id, data: { ...column } });
 
   const dndKitColumnStyle = {
     // touchAction: "none", // dành cho sensor default Pointersensor
     // dùng Translate không bị bug
     transform: CSS.Translate.toString(transform),
     transition,
+    height: "100%",
+    opacity: isDragging ? 0.5 : undefined,
   };
 
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, "_id");
+
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
   const handleClose = () => {
     setAnchorEl(null);
   };
+  // Bọc div bên ngoài để fix bug kéo thả
   return (
-    <Box
-      sx={{
-        minWidth: "300px",
-        maxWidth: "300px",
-        bgcolor: (theme) =>
-          theme.palette.mode === "dark" ? "#333643" : "#ebecf0",
-        ml: 2,
-        borderRadius: "6px",
-        height: "fit-content",
-        maxHeight: (theme) =>
-          `calc(${theme.trelloCustom.boardContentHeight} - ${theme.spacing(
-            5
-          )})`,
-      }}
-      ref={setNodeRef}
-      style={dndKitColumnStyle}
-      {...attributes}
-      {...listeners}
-    >
-      {/* Box header */}
+    <div ref={setNodeRef} style={dndKitColumnStyle} {...attributes}>
       <Box
+        {...listeners}
         sx={{
-          height: (theme) => theme.trelloCustom.columnHeaderHeight,
-          display: "flex",
-          justifyContent: "space-between",
-          p: 2,
+          minWidth: "300px",
+          maxWidth: "300px",
+          bgcolor: (theme) =>
+            theme.palette.mode === "dark" ? "#333643" : "#ebecf0",
+          ml: 2,
+          borderRadius: "6px",
+          height: "fit-content",
+          maxHeight: (theme) =>
+            `calc(${theme.trelloCustom.boardContentHeight} - ${theme.spacing(
+              5
+            )})`,
         }}
       >
-        <Typography
-          variant="h6"
+        {/* Box header */}
+        <Box
           sx={{
-            fontSize: "1rem",
-            fontWeight: "bold",
-            cursor: "pointer",
+            height: (theme) => theme.trelloCustom.columnHeaderHeight,
+            display: "flex",
+            justifyContent: "space-between",
+            p: 2,
           }}
         >
-          {column?.title}
-        </Typography>
-        <Box>
-          <Tooltip title="More options">
-            <KeyboardArrowDownIcon
-              id="basic-column-dropdown"
-              aria-controls={open ? "basic-menu-column-dropdown" : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? "true" : undefined}
-              onClick={handleClick}
-              sx={{ color: "text.primary", cursor: "pointer" }}
-            />
-          </Tooltip>
-          <Menu
-            id="basic-menu-column-dropdown"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-column-dropdown",
+          <Typography
+            variant="h6"
+            sx={{
+              fontSize: "1rem",
+              fontWeight: "bold",
+              cursor: "pointer",
             }}
           >
-            <MenuItem>
-              <ListItemIcon>
-                <AddCardIcon fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Add new card</ListItemText>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <ContentCut fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Cut</ListItemText>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <ContentCopyIcon fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Copy</ListItemText>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <ContentPasteIcon fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Paste</ListItemText>
-            </MenuItem>
-            <Divider />
-            <MenuItem>
-              <ListItemIcon>
-                <DeleteOutlineIcon fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Remove this column</ListItemText>
-            </MenuItem>
-            <MenuItem>
-              <ListItemIcon>
-                <Cloud fontSize="small" />
-              </ListItemIcon>
-              <ListItemText>Archeive this column</ListItemText>
-            </MenuItem>
-          </Menu>
+            {column?.title}
+          </Typography>
+          <Box>
+            <Tooltip title="More options">
+              <KeyboardArrowDownIcon
+                id="basic-column-dropdown"
+                aria-controls={open ? "basic-menu-column-dropdown" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                onClick={handleClick}
+                sx={{ color: "text.primary", cursor: "pointer" }}
+              />
+            </Tooltip>
+            <Menu
+              id="basic-menu-column-dropdown"
+              anchorEl={anchorEl}
+              open={open}
+              onClose={handleClose}
+              MenuListProps={{
+                "aria-labelledby": "basic-column-dropdown",
+              }}
+            >
+              <MenuItem>
+                <ListItemIcon>
+                  <AddCardIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Add new card</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentCut fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Cut</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentCopyIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Copy</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <ContentPasteIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Paste</ListItemText>
+              </MenuItem>
+              <Divider />
+              <MenuItem>
+                <ListItemIcon>
+                  <DeleteOutlineIcon fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Remove this column</ListItemText>
+              </MenuItem>
+              <MenuItem>
+                <ListItemIcon>
+                  <Cloud fontSize="small" />
+                </ListItemIcon>
+                <ListItemText>Archeive this column</ListItemText>
+              </MenuItem>
+            </Menu>
+          </Box>
+        </Box>
+
+        {/* Box list  */}
+        <ListCards cards={orderedCards} />
+        {/* Box footer */}
+        <Box
+          sx={{
+            height: (theme) => theme.trelloCustom.columFooterHeight,
+            p: 2,
+            display: "flex",
+            justifyContent: "space-between",
+            p: 2,
+          }}
+        >
+          <Button startIcon={<AddCardIcon fontSize="small" />}>
+            Add new card
+          </Button>
+          <Tooltip title="Drag to move">
+            <DragHandleIcon sx={{ cursor: "pointer" }} />
+          </Tooltip>
         </Box>
       </Box>
-
-      {/* Box list  */}
-      <ListCards cards={orderedCards} />
-      {/* Box footer */}
-      <Box
-        sx={{
-          height: (theme) => theme.trelloCustom.columFooterHeight,
-          p: 2,
-          display: "flex",
-          justifyContent: "space-between",
-          p: 2,
-        }}
-      >
-        <Button startIcon={<AddCardIcon fontSize="small" />}>
-          Add new card
-        </Button>
-        <Tooltip title="Drag to move">
-          <DragHandleIcon sx={{ cursor: "pointer" }} />
-        </Tooltip>
-      </Box>
-    </Box>
+    </div>
   );
 }
 
