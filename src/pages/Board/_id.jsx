e00641728cebab9923c76ce23383beff7bc46fd6
nghia <ntrungnghia102@gmail.com>
2024-10-24 17:10:39 +0700

    24/10

--- src/pages/Board/_id.jsx
+++ src/pages/Board/_id.jsx
@@ -1,15 +1,25 @@
-import { Container } from "@mui/material";
-import AppBar from "~/components/AppBar/AppBar";
-import BoardBar from "./BoardBar/BoardBar";
-import BoardContent from "./BoardContent/BoardContent";
-import { mockData } from "~/apis/mock-data";
+import { Container } from '@mui/material';
+import AppBar from '~/components/AppBar/AppBar';
+import BoardBar from './BoardBar/BoardBar';
+import BoardContent from './BoardContent/BoardContent';
+import { useEffect, useState } from 'react';
+import { fetchBoardDetailsAPI } from '~/apis';
 
 function Board() {
+  const [board, setBoard] = useState(null);
+
+  useEffect(() => {
+    const boardId = '6719bef267a445901b98b17c';
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board);
+    });
+  }, []);
+
   return (
-    <Container disableGutters maxWidth={false} sx={{ height: "100vh" }}>
+    <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   );
 }
